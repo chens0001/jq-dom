372ffc7f3dae4e89371808231c14a0648c939048
mac <mac@macdeMacBook-Air.local>
2022-04-26 22:10:09 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,7 @@
+
+// const $div = $('<div><span>1</span></div>')
+// const $childList = $('.child')
+// $('body').append($childList)
+//jQuery('.test').addClass('pink')
+
+$('#test').find('.child').addClass('red') 
